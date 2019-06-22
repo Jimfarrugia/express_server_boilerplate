930b01b37bffb1a8af6c26bc473eec2d60d81dfa
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-06-22 19:17:35 +1000

    added pages_controller to handle non-resource routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const pagesController = require("./../controllers/pages_controller");
 
-router.get("/", (req, res) => res.render("pages/homepage"));
+router.get("/", pagesController.homepage);
 
 module.exports = router;
